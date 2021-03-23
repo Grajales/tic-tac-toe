3240a5069ef62ad4f653410cba10e6610b8e98ec
LG29878 <grajalesliliana@johndeere.com>
2021-03-22 20:44:06 -0500

    added others solutions

---
+++ scriptLG.js
@@ -0,0 +1,21 @@
+function setName(e) {
+    e.preventDefault();
+    console.log(e);
+    alert('Success');
+
+    }
+console.log("hello")
+let box1 = document.querySelector("#box1")
+//let contain = document.querySelector(".container")
+// let button2 = document.querySelector(".button2")
+// let button3 = document.querySelector(".button3")
+// let button4 = document.querySelector(".button4")
+// let button5 = document.querySelector(".button5")
+// let button6 = document.querySelector(".button6")
+
+box1.addEventListener('click', setName)
+// button2.addEventListener('click', setName)
+// button3.addEventListener('click', setName)
+// button4.addEventListener('click', setName)
+// button5.addEventListener('click', setName)
+// button6.addEventListener('click', setName)
